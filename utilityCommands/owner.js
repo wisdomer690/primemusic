98a5ca4dabd14f393bcbee94f368f1861ac235fa
adamRazor <adamgomdz12@gmail.com>
2025-03-13 04:56:33 +0000

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
+        const youtubeLink = 'https://www.youtube.com/@Apollo_belvedere';
+        const InstagramLink = 'https://www.instagram.com/apollo_bevedere2?igsh=aXFyam5iMWl2ZzZ0';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖 apollo belvedere or stalin. I am a discord bot developer. I love playing guitar, watching anime and building different discord bots. Feel free to contact me!\n ❤️ [Apollo](${youtubeLink})\n 💙 [stalinbe](${InstagramLink})`)
             .setTimestamp();
 
 
