1ff621e020112e3a5098d0488fbca660f2cf5908
adamRazor <adamgomdz12@gmail.com>
2025-03-13 04:46:16 +0000

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -5,18 +5,18 @@ module.exports = {
     description: 'support server of this Bot',
     execute(message, args) {
         const supportServerLink = "https://discord.gg/aJTv9Wf5";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
+      const githubLink = "https://github.com/razor23gtk";
+      const replitLink = "https://discord.gg/QGMKQd9NJW";
       const youtubeLink = "https://www.youtube.com/@Apollo_belvedere";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://discord.gg/aJTv9Wf5'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setImage('https://files.shapes.inc/4586febf.png')
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
