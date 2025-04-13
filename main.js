8d267473176e2436b0db1d14fc1f37db4007e20f
wisdomer690 <c.arljhon879@gmail.com>
2025-04-13 05:43:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -108,8 +108,8 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `apolloBelvedere`, type: ActivityType.Watching }],
-      status: 'idle',
+      activities: [{ name: ` Wisdom Supporters 🚀 `, type: ActivityType.Watching }],
+      status: 'online',
     });
   }, 2000); 
 });
